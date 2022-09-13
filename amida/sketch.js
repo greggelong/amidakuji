6b8051f97335c0f4b4d99be32ba1907b14ab5f31
Greg K <gkreisman_cep@qq.com>
2022-09-13 10:05:50 +0800

    p5 sketch

---
+++ amida/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(600,600)
+  background(0)
+}
+
+function draw() {
+  // put drawing code here
+  fill(255)
+  ellipse(mouseX,mouseY,100)
+}
