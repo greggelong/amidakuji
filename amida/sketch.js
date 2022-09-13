8423b7c7e84eab5fd206e2be6e62970649907971
Greg K <gkreisman_cep@qq.com>
2022-09-13 12:44:37 +0800

    random horizontals

--- amida/sketch.js
+++ amida/sketch.js
@@ -1,4 +1,4 @@
-grid = [
+let grid = [
     [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0],
@@ -18,6 +18,8 @@ grid = [
     [0,0,0,0,0]
   ]
 
+let rows =17
+let cols =5
 
 
 
@@ -25,7 +27,8 @@ function setup() {
   // put setup code here
   createCanvas(600,600)
   background(0)
-  print(grid)
+  putHorizontals();
+  console.table(grid)
 }
 
 function draw() {
@@ -34,3 +37,27 @@ function draw() {
   ellipse(mouseX,mouseY,100)
 }
 
+
+function putHorizontals(){
+    for(let x= 0;x<cols-1;x++){
+        for(let y=1;y<rows-1;y+=2){
+            //first col case
+            if (x == 0){
+                r = floor(random(1,3) ) 
+                if (r ==2){
+                    grid[y][x]=1
+                    grid[y][x+1]=1
+                }
+            }
+            //other col case
+
+            else if(grid[y][x]!=1 && grid[y][x-1] !=1){
+                r = floor(random(1,3))
+                if(r ==2){
+                    grid[y][x]=1
+                    grid[y][x+1]=1
+                }
+            }
+        }
+    }
+}
